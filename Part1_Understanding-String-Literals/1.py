58cb258d1db7c7e6a4bc652e3ef7221cc87c537e
Fiza Awan <awanfiza95@gmail.com>
2025-03-22 07:35:29 +0500

    updated

---
+++ Part1_Understanding-String-Literals/1.py
@@ -0,0 +1,5 @@
+# **1. Creating strings using different quotation types**
+
+single_quote = 'This is a string using single quotes.'
+double_quote = "This is a string using double quotes."
+triple_quote = """This is a string using triple quotes."""
